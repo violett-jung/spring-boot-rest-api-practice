d25cb88274f4131cabc59877cb587706c2950a14
violett-jung <violett.jung@gmail.com>
2023-04-13 02:21:55 +0900

    id와 일치하는 회원 조회하는 getMember 메소드 추가
    - MemberController 내에 getMember 메소드 추가

--- src/main/java/com/study/restapipractice/controller/MemberController.java
+++ src/main/java/com/study/restapipractice/controller/MemberController.java
@@ -6,11 +6,12 @@ import com.study.restapipractice.entity.MemberEntity;
 import com.study.restapipractice.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -35,9 +36,17 @@ public class MemberController {
     public List<MemberEntity> getMembers(){
         List<MemberEntity> members = memberRepository.findAll();
         log.info(members.toString());
+
         return members;
     }
 
+    //03-1-2. 회원조회
+    //need: 예외처리-id에 해당하는 회원이 없는 경우
+    @GetMapping("/account/{id}")
+    public MemberEntity getMember(@PathVariable("id") Long seq){
+        MemberEntity memberEntity = memberRepository.findById(seq).get();
+        return memberEntity;
+    }
 
     //03-2. post : 로그인
     //03-3. post : 회원등록
